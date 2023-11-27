e613ba4554e894f8cefc9ab496bb9d7c59aff086
S-a-a-h <sam_jb_2@hotmail.co.uk>
2023-11-27 11:33:32 +0000

    merge conflict testing on main

--- example.py
+++ example.py
@@ -1,7 +1,6 @@
-import time 
+import time
 
-#this is an example python script to test merge conflicts
-time.sleep(1)
-print(f'Hello World!, the time is {time.time()}')
-time.sleep(1)
-
+# This is an example python script to test merge conflicts
+time.sleep(3)
+print(f"Hello World!, the time is {time.time()}")
+time.sleep(3)
